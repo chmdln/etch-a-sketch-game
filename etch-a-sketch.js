d162ed13399f2ae2a6efb602bff8a4e4d49c0061
tatyana <beatytanya@gmail.com>
2024-10-05 09:08:13 -0400

    Create 16 x 16 flexbox grid

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,1 +1,13 @@
-console.log('Hello'); 
+const containerElem = document.querySelector('.container'); 
+
+for (let i=0; i<16; i++) {
+    for (let j=0; j<16; j++) {
+        let squareElem = document.createElement('div');  
+        squareElem.style.width = `${600/16-1}px`; 
+        squareElem.style.height = `${600/16-1}px`; 
+        squareElem.style.borderStyle= 'solid'; 
+        squareElem.style.borderWidth= '0.0001px'; 
+        squareElem.style.borderColor = 'darkgrey'; 
+        containerElem.appendChild(squareElem);
+    }
+}
